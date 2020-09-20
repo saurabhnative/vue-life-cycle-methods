1f6b2cf36be78b052cf2170778d12ce33d388f2e
codeclassifiers <codeclassifiers@gmail.com>
2020-09-20 21:11:37 +0530

    first

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>BluePrint Status: {{planStatus}}</p>
+    <button @click="changePlanStatus">Change plan status</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      planStatus: 'In Progress',
+      sponsor: 'Yet to be finalised'
+    };
+  },
+  beforeCreate() {
+    // alert("Status of conference plan: "+this.planStatus);
+  },
+  created() {
+    // alert("In created hook, initial status of conference plan is : " + this.planStatus);
+    this.planStatus = 'Blueprint finalised';
+    // alert("In created hook, updated status of conference plan is: " + this.planStatus);
+  },
+  beforeMount() {
+    // Print value of dom element 
+    // console.log("Values of this.$el in beforeMount", this.$el);
+  },
+  mounted() {
+    // Print value of dom element 
+    // console.log("Values of this.$el in mounted", this.$el);
+  },
+  beforeUpdate() {
+    // console.log("Value of plan status in beforeUpdate(): " + this.planStatus);
+  },
+  updated() {
+    console.log("Value of plan status in updated():" + this.planStatus);
+  },
+  beforeDestroy() {
+    this.planStatus = null;
+    delete this.planStatus;
+  },
+  methods: {
+    changePlanStatus() {
+      // do smth
+      this.planStatus = 'Event speaker changed';
+    },
+  },
+};
 </script>
 
 <style>
